2f6c13837b2db0a7900269a9bb783f33b9aa0d18
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-03-12 23:09:30 +0100

    update port for node server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
